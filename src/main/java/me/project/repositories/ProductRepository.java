f36972597d4419d191cfc0ebfa1b3e1655dbf2aa
PimpinFrito <fritzt14@icloud>
2024-05-05 14:38:12 -0400

    first commit

--- src/main/java/me/project/repositories/ProductRepository.java
+++ src/main/java/me/project/repositories/ProductRepository.java
@@ -1,4 +1,10 @@
 package me.project.repositories;
 
+import me.project.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ProductRepository {
+
+    Page<Product> findByCategoryId(Long id, Pageable pageable);
 }
