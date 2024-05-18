4228fa9905168e6b12fadb835aa22917053e0cb0
PimpinFrito <fritzt14@icloud>
2024-05-18 16:07:03 -0400

    Added Country and State and the rest endpoints. Updated rest config to block any access other than read to new entity endpoints

--- src/main/java/me/project/config/DataRestConfig.java
+++ src/main/java/me/project/config/DataRestConfig.java
@@ -1,8 +1,10 @@
 package me.project.config;
 
 import jakarta.persistence.EntityManager;
+import me.project.entity.Country;
 import me.project.entity.Product;
 import me.project.entity.ProductCategory;
+import me.project.entity.State;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -12,32 +14,49 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
 
-    private EntityManager entityManager;
-
-    public DataRestConfig(EntityManager entityManager){
-        this.entityManager = entityManager;
-    }
-
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
+        disableHttpMethods(Product.class, config, theUnsupportedActions);
+        disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+        disableHttpMethods(Country.class, config, theUnsupportedActions);
+        disableHttpMethods(State.class, config, theUnsupportedActions);
 
         // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
-        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+//        config.getExposureConfiguration()
+//                .forDomainType(Product.class)
+//                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+//                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+//
+//        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
+//        config.getExposureConfiguration()
+//                .forDomainType(ProductCategory.class)
+//                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+//                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+//
+//        config.getExposureConfiguration()
+//                .forDomainType(Country.class)
+//                .withItemExposure((metadata, httpMethods) -> httpMethods.disable((theUnsupportedActions)))
+//                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
         //Expose ids for sidebar menu
-        config.exposeIdsFor(ProductCategory.class);
+        this.exposeIds(config);
 
+    }
+
+    private void  disableHttpMethods(Class theClass, RepositoryRestConfiguration config,  HttpMethod[] unsupportedActions){
+
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config){
+        config.exposeIdsFor(ProductCategory.class);
+        config.exposeIdsFor(Product.class);
     }
 
 }
